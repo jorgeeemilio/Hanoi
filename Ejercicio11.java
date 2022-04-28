7dfb62cae491121905e6a7265c25656baa1bbb26
María José <mjmartinezstudium@gmail.com>
2022-04-28 10:57:02 +0200

    Ejercicio 11 con contador

--- Ejercicio11.java
+++ Ejercicio11.java
@@ -7,25 +7,31 @@ public class Ejercicio11
 	public static void main(String[] args)
 	{
 		int discos;
+		int contador = 0;
 		Scanner teclado = new Scanner(System.in);
 		System.out.println("Introducir el número de discos a procesar:");
 		discos = teclado.nextInt();
-		// Hanoi(número de discos, columna origen, columna destino, auxiliar)
-		Hanoi(discos, 1, 3, 2);
 		teclado.close();
+		// Hanoi(número de discos, columna origen, columna destino, auxiliar)
+		contador = Hanoi(discos, 1, 3, 2, contador);
+		System.out.println("Se han realizado " + contador + " movimientos.");
 	}
 	
-	public static void Hanoi(int n, int x, int y, int aux)
+	public static int Hanoi(int n, int x, int y, int aux, int c)
 	{
 		if(n==1)
 		{
 			System.out.println("Mover el disco de la torre "+x+" a la torre	"+y);
+			c++;
+			return(c);
 		}
 		else
 		{
-			Hanoi(n-1,x,aux,y);
+			c = Hanoi(n-1, x, aux, y, c);
 			System.out.println("Mover el disco de la torre "+x+" a la torre	"+y);
-			Hanoi(n-1,aux,y,x);
+			c = Hanoi(n-1, aux, y, x, c);
+			c++;
+			return(c);
 		}
 	}
 }
